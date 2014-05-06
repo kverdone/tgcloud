7c471f1792e8212b3ff81a6f115c0cd10219e275
kverdone <kverdone@gmail.com>
2014-05-05 22:57:33 -0700

    Added User View

--- tgcloud.py
+++ tgcloud.py
@@ -3,7 +3,12 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-  return 'Future cloud placement.'
+  return 'Empty Home Page'
+
+@app.route('/user/<username>')
+def cloud(username):
+  return 'Future word cloud for {0}.'.format(username)
+
 
 if __name__ == '__main__':
-  app.run()
+  app.run(debug=True)
