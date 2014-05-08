2f3c6ce6b7c24c1ece3543c6c6b74def42db1968
kverdone <kverdone@gmail.com>
2014-05-08 01:18:01 -0700

    Clouds built, pulling from source data

--- tgcloud.py
+++ tgcloud.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template
-from config import query
+from config import query,wordlist
 app = Flask(__name__)
 
 @app.route('/')
@@ -7,13 +7,23 @@ def home():
   return render_template('index.html')
 
 @app.route('/user/<username>')
-def cloud(username):
+def mycloud(username):
   return render_template('user.html', username=username.lower())
 
 @app.route('/recent/<username>')
 def recent(username):
   entries = query(username)
   return render_template('recent.html', username=username, entries=entries)
+
+@app.route('/cloud/<username>')
+def jdcloud(username):
+  dict = wordlist(username)
+  return render_template('jdcloud.html', username=username, entries=dict)
+
+@app.route('/<username>')
+def cloud(username):
+  dict = wordlist(username)
+  return render_template('cloud.html', username=username, entries = dict)
 
 if __name__ == '__main__':
   app.run(debug=True)
