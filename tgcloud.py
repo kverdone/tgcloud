c1c5e71aac44797e2dee9a11f8130e8e1ecade84
kverdone <kverdone@gmail.com>
2014-05-09 01:19:54 -0700

    Added WTForms and bootstrap, probably don't need WTForms

--- tgcloud.py
+++ tgcloud.py
@@ -1,10 +1,24 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, session, redirect, url_for
 from config import query,wordlist
-app = Flask(__name__)
+from flask.ext.wtf import Form
+from wtforms import StringField, SubmitField
+from wtforms.validators import Required
+from flask.ext.bootstrap import Bootstrap
 
-@app.route('/')
+
+app = Flask(__name__)
+bootstrap = Bootstrap(app)
+app.config['SECRET_KEY'] = 'qwertyuiopasdfghjklzxcvbnm'
+
+@app.route('/', methods=['GET','POST'])
 def home():
-  return render_template('index.html')
+  form = NameForm()
+  print session.get('name')
+  if form.validate_on_submit():
+    session['name'] = form.name.data
+    print session.get('name')
+    return redirect(url_for('home'))
+  return render_template('index.html', form=form, name=session.get('name'))
 
 @app.route('/user/<username>')
 def mycloud(username):
@@ -24,6 +38,11 @@ def jdcloud(username):
 def cloud(username):
   dict = wordlist(username)
   return render_template('cloud.html', username=username, entries = dict)
+
+class NameForm(Form):
+  name = StringField('What is your name?', validators=[Required()])
+  submit = SubmitField('Submit')
+
 
 if __name__ == '__main__':
   app.run(debug=True)
